a12a027729e9d391c1f465ecd44262d6ea4bd202
Oscar Reynoso <kokihidalgo18@gmail.com>
2024-10-14 10:16:14 -0400

    Add database connection setup using GORM and SQLite

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,7 +1,20 @@
 package config
 
 import (
+	"backend/models"
+	"log"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"log"
 )
+
+var DB *gorm.DB //stores DB connection
+
+func ConnectDatabase() {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{}) //open conn
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB = db //store the conn
+	DB.AutoMigrate(&models.Flashcard{}, &models.Review{})
+}
