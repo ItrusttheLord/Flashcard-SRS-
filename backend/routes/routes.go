a5c76d701ab8516e6851752b0821f8af68007223
Oscar Reynoso <kokihidalgo18@gmail.com>
2024-10-15 13:21:07 -0400

    Updated Routes for the Learning plan

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -11,4 +11,8 @@ var GetRoutes = func(router *mux.Router) {
 	router.HandleFunc("/flashcards", controllers.CreateCard).Methods("POST")
 	router.HandleFunc("/flashcards/{id}", controllers.UpdateCard).Methods("PUT")
 	router.HandleFunc("/flashcards/{id}", controllers.DeleteCard).Methods("DELETE")
+	// routes for gettin learning plan
+	router.HandleFunc("/learning-plan", controllers.CreateLearningPlan).Methods("POST")
+	router.HandleFunc("/learning-plan", controllers.GetLearningPlans).Methods("GET")
+	router.HandleFunc("/learning-plan/{id}", controllers.UpdateLearningPlan).Methods("PUT")
 }
